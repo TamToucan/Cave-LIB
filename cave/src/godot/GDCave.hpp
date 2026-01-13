8268e14376db417a2a4dc0a597a6e3cee7b61fa4
Tam Toucan <dr@tamtoucan.com>
2026-01-13 09:39:32 +0000

    Fixup include paths for godot so antigravity can find headers

--- cave/src/godot/GDCave.hpp
+++ cave/src/godot/GDCave.hpp
@@ -4,51 +4,52 @@
 #include <godot_cpp/classes/object.hpp>
 #include <godot_cpp/classes/tile_map_layer.hpp>
 #include <vector>
+
 #include "core/CaveInfo.h"
 #include "core/GenerationParams.h"
 #include "core/TileTypes.h"
 
+
 namespace godot {
 
 class GDCave : public Object {
-	GDCLASS(GDCave, Object)
+  GDCLASS(GDCave, Object)
 
-protected:
-	static void _bind_methods();
+ protected:
+  static void _bind_methods();
 
-	Cave::CaveInfo m_cave_info;
-    Cave::GenerationParams m_gen_params;
-	std::vector<std::vector<int>> m_tile_map;
+  Cave::CaveInfo m_cave_info;
+  Cave::GenerationParams m_gen_params;
+  std::vector<std::vector<int>> m_tile_map;
 
-    godot::Vector2i m_floor_tile;
-    godot::Vector2i m_wall_tile;
+  godot::Vector2i m_floor_tile;
+  godot::Vector2i m_wall_tile;
 
+ public:
+  GDCave();
+  ~GDCave();
 
-public:
-	GDCave();
-	~GDCave();
+  GDCave* setCaveSize(Vector2i caveSize);
+  GDCave* setBorderCellSize(Vector2i border);
+  GDCave* setCellSize(Vector2i cellSize);
+  GDCave* setStartCell(int x, int y);
+  GDCave* setFloor(godot::Vector2i floor);
+  GDCave* setWall(godot::Vector2i wall);
+  GDCave* setOctaves(int octaves);
+  GDCave* setPerlin(bool usePerlin);
+  GDCave* setWallChance(float wallChance);
+  GDCave* setFreq(float freq);
+  GDCave* setAmp(float amp);
+  GDCave* setGenerations(const godot::Array& gens);
 
-	GDCave* setCaveSize(Vector2i caveSize);
-	GDCave* setBorderCellSize(Vector2i border);
-	GDCave* setCellSize(Vector2i cellSize);
-	GDCave* setStartCell(int x, int y);
-	GDCave* setFloor(godot::Vector2i floor);
-	GDCave* setWall(godot::Vector2i wall);
-	GDCave* setOctaves(int octaves);
-	GDCave* setPerlin(bool usePerlin);
-	GDCave* setWallChance(float wallChance);
-	GDCave* setFreq(float freq);
-	GDCave* setAmp(float amp);
-	GDCave* setGenerations(const godot::Array& gens);
+  void make_cave(TileMapLayer* pTileMap, int layer, int seed);
 
-	void make_cave(TileMapLayer* pTileMap, int layer, int seed);
-
-private:
-    void copy_core_to_tilemap(TileMapLayer* pTileMap, int layer, const Cave::TileMap& caveMap);
-    Vector2i map_tilename_to_vector2i(Cave::TileName tile_name);
-    void setCell(TileMapLayer* pTileMap, int layer, int x, int y, Vector2i tile);
+ private:
+  void copy_core_to_tilemap(TileMapLayer* pTileMap, int layer, const Cave::TileMap& caveMap);
+  Vector2i map_tilename_to_vector2i(Cave::TileName tile_name);
+  void setCell(TileMapLayer* pTileMap, int layer, int x, int y, Vector2i tile);
 };
 
-}
+}  // namespace godot
 
 #endif
